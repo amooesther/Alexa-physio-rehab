38fc0163d65e029e91857ce02c258f1b63c7adc8
Esther Toyin <amooesther2503@gmail.com>
2024-05-23 14:35:22 +0100

    testimonial

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -12,7 +12,7 @@ const Testimonial = () => {
     {
       id: 2,
       name: 'Oluwapelumi Omoleke',
-      text: 'They have one of the best hands.I would highly recommend this place to anyone in need of physiotherapy services anytime anyday. Very impressive service delivery with positive results!!!!',
+      text: 'They have one of the best hands. I would highly recommend this place to anyone in need of physiotherapy services anytime anyday. Very impressive service delivery with positive results!!!!',
     },
     {
       id: 3,
@@ -27,7 +27,7 @@ const Testimonial = () => {
     {
       id: 5,
       name: 'Oluwapelumi Omoleke',
-      text: 'They have one of the best hands.I would highly recommend this place to anyone in need of physiotherapy services anytime anyday. Very impressive service delivery with positive results!!!!',
+      text: 'They have one of the best hands. I would highly recommend this place to anyone in need of physiotherapy services anytime anyday. Very impressive service delivery with positive results!!!!',
     },
     {
       id: 6,
@@ -45,19 +45,23 @@ const Testimonial = () => {
   }, [startIndex]);
 
   const handleNext = () => {
-    setStartIndex((prevIndex) => (prevIndex + 1) % (testimonials.length - 2));
+    setStartIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
   };
 
   const handlePrev = () => {
-    setStartIndex((prevIndex) => (prevIndex === 0 ? testimonials.length - 3 : prevIndex - 1));
+    setStartIndex((prevIndex) => (prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1));
   };
 
   return (
     <div className='testimony'>
       <h3>See what some of our patients have to say</h3>
       <div className="testimonial-container">
-        {testimonials.slice(startIndex, startIndex + 3).map((testimonial) => (
-          <div key={testimonial.id} className="testimonial-card">
+        {testimonials.map((testimonial, index) => (
+          <div
+            key={testimonial.id}
+            className="testimonial-card"
+            style={{ display: (index >= startIndex && index < startIndex + 3) ? 'block' : 'none' }}
+          >
             <div className="testimonial-text">
               <p>{testimonial.text}</p>
             </div>
@@ -67,13 +71,8 @@ const Testimonial = () => {
           </div>
         ))}
       </div>
-      <div className="arrow-container">
-        <button className="prev-arrow" onClick={handlePrev}>&#8249;</button>
-        <button className="next-arrow" onClick={handleNext}>&#8250;</button>
-      </div>
-      <div><iframe width="100%" height="280" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=280&amp;hl=en&amp;q=3,%20Ado%20street%20ijapo%20Estate%20Akure%20ondo%20state+(Alexa%20Physiotherapy%20and%20Rehabilitation%20Clinic)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps devices</a></iframe></div>
     </div>
   );
-};
+}
 
 export default Testimonial;
