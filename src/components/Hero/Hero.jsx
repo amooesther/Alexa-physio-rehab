efa1053be88c534fd528f4c2a7801d03082ae3ae
Esther Toyin <amooesther2503@gmail.com>
2024-05-23 13:46:18 +0100

    css adjustments

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import "./Hero.css"
-import heroPicture from '../../Assets/heroPicture.png'
+import React from "react";
+import "./Hero.css";
+import heroPicture from "../../Assets/heroPicture.png";
 
 const Hero = () => {
   return (
-    <div className='heroWrapper'>
-    <img src={heroPicture} alt="" />
-        <div className='heroLeft'>
-            <h1>
-            Welcome to Alexa physiotherapy and Rehabilitation clinic,
-               
-            </h1>
-            <p>  A conservative way of treatment, guiding you towards optimal health and well-being </p>
-            <button className='heroBtn'>Book Appointment</button>
-        </div>
-        
-
+    <div className="heroWrapper">
+      <img src={heroPicture} alt="" />
+      <div className="heroLeft">
+        <h1>Welcome to Alexa physiotherapy and Rehabilitation clinic,</h1>
+        <p>
+         
+          A conservative way of treatment, guiding you towards optimal health
+          and well-being
+        </p>
+        <button className="heroBtn">Book Appointment</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../Assets/logo192.png';
 import "./NavBar.css"
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className='NavWrapper'>
       <img src={logo} alt="Logo" className="logo" />
-      <nav>
-        <ul className="nav-links">
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+      <nav className={`nav-links ${menuOpen ? 'open' : ''}`}>
+        <ul>
           <li><a href="#">Home</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Services</a></li>
           <li><a href="#">Contact</a></li>
           <button className='loginBtn'>Login</button>
         </ul>
-       
       </nav>
     </div>
   );
